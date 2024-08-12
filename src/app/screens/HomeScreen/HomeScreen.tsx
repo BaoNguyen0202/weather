2d65904dad6b09da81606c10a3e8ff4535cda8b9
baonguyen <bao17030202@gmail.com>
2024-08-12 17:01:54 +0700

    +commit

--- src/app/screens/HomeScreen/HomeScreen.tsx
+++ src/app/screens/HomeScreen/HomeScreen.tsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import React, { } from 'react'
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, FlatList, Animated, Dimensions } from 'react-native';
+import React, { useRef } from 'react';
 import { useTheme } from '../../layouts/theme';
 import { rootStyles } from './style';
 import LinearGradient from 'react-native-linear-gradient';
@@ -10,18 +10,23 @@ import { Image } from 'react-native';
 import Province from './data.json';
 import { normalizeText, updateCurrentDate } from '../../services/AppService';
 
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+const { height } = Dimensions.get('window');
+
 const HomeScreen = () => {
     const theme = useTheme();
     const styles = rootStyles(theme);
     const [searchText, setSearchText] = React.useState('');
     const [data, setData] = React.useState<any>({});
     const [currentDate, setCurrentDate] = React.useState<string>('');
-    const [filteredData, setFilteredData] = React.useState<any>({})
+    const [filteredData, setFilteredData] = React.useState<any[]>([]);
+    const scrollY = useRef(new Animated.Value(0)).current;
 
     const fetchData = React.useCallback(async (lat: number, lon: number) => {
         try {
             const response: any = await AppService.getWeather(lat, lon);
             setData(response);
+            setFilteredData([response]);
         } catch (error) {
             console.error(error);
         }
@@ -33,24 +38,139 @@ const HomeScreen = () => {
         const filtered = Province.filter(item =>
             normalizeText(item.name).includes(normalizedSearchText)
         );
-        filtered.forEach(item => {
-            setFilteredData(item);
-            // const intervalId = setInterval(() => {
-            //     fetchData(parseFloat(item.lat), parseFloat(item.lon));
-            // }, 60000);
-            // return () => clearInterval(intervalId);
-        });
+        setFilteredData(filtered);
     };
 
     React.useEffect(() => {
         setCurrentDate(updateCurrentDate());
         fetchData(21.0285, 105.8542);//vị trí hiện tại
-        // const intervalId = setInterval(() => {
-        //     fetchData(21.0285, 105.8542);
-        // }, 60000);
-
-        // return () => clearInterval(intervalId);
     }, []);
+
+    const renderItem = ({ item, index }: any) => {
+        const inputRange = [
+            (index - 1) * height,
+            index * height,
+            (index + 0.5) * height,
+            (index + 1) * height
+        ];
+        const opacity = scrollY.interpolate({
+            inputRange,
+            outputRange: [0, 1, 1, 0]
+        });
+        const scale = scrollY.interpolate({
+            inputRange,
+            outputRange: [0.8, 1, 1, 0.8]
+        });
+
+        return (
+            <Animated.View style={{
+                opacity,
+                transform: [{ scale }]
+            }}>
+                <Block alignItems='center'>
+                    <Image
+                        source={{ uri: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` }}
+                        style={{ width: 140, height: 140, marginBottom: -25 }}
+                    />
+                    <Text style={[styles.text, styles.textMain]}>{item.name}</Text>
+                    <Block padding={24} margin={24} paddingVertical={14} borderRadius={15} borderColor='#fff' border alignItems='center'>
+                        <Block style={styles.overlay}>
+                            <Block color='#fff' opacity={0.3} style={styles.background} />
+                        </Block>
+                        <Text style={[styles.text, styles.textDate]}>Today, {currentDate}</Text>
+                        <Text style={[styles.text, styles.textTemp]}>{Math.round(data.main.temp - 273.15)}°C</Text>
+                        <Text style={[styles.text, styles.textMain]}>{data.weather[0].main}</Text>
+                        <Block>
+                            <Block direction='row'>
+                                <Icon source="weather-windy" size={20} color="#fff" />
+                                <Text style={[styles.text, styles.textWindHum]}>     Wind     |     {data.wind.speed} km/h</Text>
+                            </Block>
+                            <Block direction='row'>
+                                <Icon source="water-outline" size={20} color="#fff" />
+                                <Text style={[styles.text, styles.textWindHum]}>     Hum      |     {data.main.humidity}%</Text>
+                            </Block>
+                        </Block>
+                    </Block>
+                </Block>
+                <Block alignItems='center'>
+                    <Image
+                        source={{ uri: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` }}
+                        style={{ width: 140, height: 140, marginBottom: -25 }}
+                    />
+                    <Text style={[styles.text, styles.textMain]}>{item.name}</Text>
+                    <Block padding={24} margin={24} paddingVertical={14} borderRadius={15} borderColor='#fff' border alignItems='center'>
+                        <Block style={styles.overlay}>
+                            <Block color='#fff' opacity={0.3} style={styles.background} />
+                        </Block>
+                        <Text style={[styles.text, styles.textDate]}>Today, {currentDate}</Text>
+                        <Text style={[styles.text, styles.textTemp]}>{Math.round(data.main.temp - 273.15)}°C</Text>
+                        <Text style={[styles.text, styles.textMain]}>{data.weather[0].main}</Text>
+                        <Block>
+                            <Block direction='row'>
+                                <Icon source="weather-windy" size={20} color="#fff" />
+                                <Text style={[styles.text, styles.textWindHum]}>     Wind     |     {data.wind.speed} km/h</Text>
+                            </Block>
+                            <Block direction='row'>
+                                <Icon source="water-outline" size={20} color="#fff" />
+                                <Text style={[styles.text, styles.textWindHum]}>     Hum      |     {data.main.humidity}%</Text>
+                            </Block>
+                        </Block>
+                    </Block>
+                </Block>
+                <Block alignItems='center'>
+                    <Image
+                        source={{ uri: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` }}
+                        style={{ width: 140, height: 140, marginBottom: -25 }}
+                    />
+                    <Text style={[styles.text, styles.textMain]}>{item.name}</Text>
+                    <Block padding={24} margin={24} paddingVertical={14} borderRadius={15} borderColor='#fff' border alignItems='center'>
+                        <Block style={styles.overlay}>
+                            <Block color='#fff' opacity={0.3} style={styles.background} />
+                        </Block>
+                        <Text style={[styles.text, styles.textDate]}>Today, {currentDate}</Text>
+                        <Text style={[styles.text, styles.textTemp]}>{Math.round(data.main.temp - 273.15)}°C</Text>
+                        <Text style={[styles.text, styles.textMain]}>{data.weather[0].main}</Text>
+                        <Block>
+                            <Block direction='row'>
+                                <Icon source="weather-windy" size={20} color="#fff" />
+                                <Text style={[styles.text, styles.textWindHum]}>     Wind     |     {data.wind.speed} km/h</Text>
+                            </Block>
+                            <Block direction='row'>
+                                <Icon source="water-outline" size={20} color="#fff" />
+                                <Text style={[styles.text, styles.textWindHum]}>     Hum      |     {data.main.humidity}%</Text>
+                            </Block>
+                        </Block>
+                    </Block>
+                </Block>
+                <Block alignItems='center'>
+                    <Image
+                        source={{ uri: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` }}
+                        style={{ width: 140, height: 140, marginBottom: -25 }}
+                    />
+                    <Text style={[styles.text, styles.textMain]}>{item.name}</Text>
+                    <Block padding={24} margin={24} paddingVertical={14} borderRadius={15} borderColor='#fff' border alignItems='center'>
+                        <Block style={styles.overlay}>
+                            <Block color='#fff' opacity={0.3} style={styles.background} />
+                        </Block>
+                        <Text style={[styles.text, styles.textDate]}>Today, {currentDate}</Text>
+                        <Text style={[styles.text, styles.textTemp]}>{Math.round(data.main.temp - 273.15)}°C</Text>
+                        <Text style={[styles.text, styles.textMain]}>{data.weather[0].main}</Text>
+                        <Block>
+                            <Block direction='row'>
+                                <Icon source="weather-windy" size={20} color="#fff" />
+                                <Text style={[styles.text, styles.textWindHum]}>     Wind     |     {data.wind.speed} km/h</Text>
+                            </Block>
+                            <Block direction='row'>
+                                <Icon source="water-outline" size={20} color="#fff" />
+                                <Text style={[styles.text, styles.textWindHum]}>     Hum      |     {data.main.humidity}%</Text>
+                            </Block>
+                        </Block>
+                    </Block>
+                </Block>
+            </Animated.View>
+        );
+    };
+
 
     return (
         <LinearGradient
@@ -73,37 +193,18 @@ const HomeScreen = () => {
                         </TouchableOpacity>
                     </Block>
                 </Block>
-                {data.main && (
-                    <Block alignItems='center'>
-                        <Image
-                            source={{ uri: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` }}
-                            style={{ width: 140, height: 140, marginBottom: -25 }}
-                        />
-                        <Text style={[styles.text, styles.textMain]}>{filteredData.name || 'Vị trí hiện tại'}</Text>
-                        <Block padding={24} margin={24} paddingVertical={14} borderRadius={15} borderColor='#fff' border alignItems='center'>
-                            <Block style={styles.overlay}>
-                                <Block color='#fff' opacity={0.3} style={styles.background} />
-                            </Block>
-                            <Text style={[styles.text, styles.textDate]}>Today, {currentDate}</Text>
-                            <Text style={[styles.text, styles.textTemp]}>{Math.round(data.main.temp - 273.15)}°C</Text>
-                            <Text style={[styles.text, styles.textMain]}>{data.weather[0].main}</Text>
-                            <Block>
-                                <Block direction='row'>
-                                    <Icon source="weather-windy" size={20} color="#fff" />
-                                    <Text style={[styles.text, styles.textWindHum]}>     Wind     |     {data.wind.speed} km/h</Text>
-                                </Block>
-                                <Block direction='row'>
-                                    <Icon source="water-outline" size={20} color="#fff" />
-                                    <Text style={[styles.text, styles.textWindHum]}>     Hum      |     {data.main.humidity}%</Text>
-                                </Block>
-                            </Block>
-                        </Block>
-                    </Block>
-                )}
+                <AnimatedFlatList
+                    data={filteredData}
+                    renderItem={renderItem}
+                    keyExtractor={(item: any) => item.name}
+                    onScroll={Animated.event(
+                        [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                        { useNativeDriver: true }
+                    )}
+                />
             </Block>
         </LinearGradient>
     );
 }
 
-export default HomeScreen
-
+export default HomeScreen;
